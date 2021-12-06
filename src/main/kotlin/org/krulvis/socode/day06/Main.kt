e747bed5dc1ad4874385de2b9a32a2c3d25270b6
Krulvis <git@krulvis.space>
2021-12-06 13:36:22 +0100

    🎄long[N]🎄

--- src/main/kotlin/org/krulvis/socode/day06/Main.kt
+++ src/main/kotlin/org/krulvis/socode/day06/Main.kt
@@ -1,19 +1,19 @@
 package org.krulvis.socode.day06
 
 fun main() {
-    var lanterns = ClassLoader.getSystemResource("day6.txt").readText().split(",").map { it.toInt() }
+    val lanterns = ClassLoader.getSystemResource("day6.txt").readText().split(",").map { it.toInt() }
     val days = 256
     optimized(lanterns, days)
 }
 
 fun optimized(lanterns: List<Int>, days: Int) {
     var lanterns = lanterns
-    val dailyNewborn = mutableMapOf<Int, Long>()
-    for (day in 1..days) {
+    val dailyNewborn = LongArray(days)
+    for (day in 0 until days) {
         lanterns = lanterns.map {
             val newVal = it - 1
             if (newVal < 0) {
-                dailyNewborn[day] = dailyNewborn.getOrDefault(day, 0) + 1
+                dailyNewborn[day] = dailyNewborn[day] + 1
                 6
             } else {
                 newVal
@@ -21,12 +21,12 @@ fun optimized(lanterns: List<Int>, days: Int) {
         }.toMutableList()
         var newbornDay = day - 9
         while (newbornDay > 0) {
-            dailyNewborn[day] = dailyNewborn.getOrDefault(day, 0) + dailyNewborn.getOrDefault(newbornDay, 0)
+            dailyNewborn[day] = dailyNewborn[day] + dailyNewborn[newbornDay]
             newbornDay -= 7
         }
         println(
             "Day $day has ${dailyNewborn[day]} new lanternfish and ${
-                lanterns.size + dailyNewborn.values.sum()
+                lanterns.size + dailyNewborn.sum()
             } total"
         )
     }
